832993a84254972d5320bd5724dba8cf92517482
Ivan <ivan.shelonik@gmail.com>
2026-08-21 20:58:11 +0200

    Add: anagram-groups - submission-9

---
+++ Data Structures & Algorithms/anagram-groups/submission-9.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # T: O(m*n) where n~100; S: O(m*n) Where m is the number of strings and n is the length of the longest string.
+        h = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            key = tuple(count) # tuples are immutable, can use as key
+            h[key].append(s)
+        return list(h.values())
+ 
