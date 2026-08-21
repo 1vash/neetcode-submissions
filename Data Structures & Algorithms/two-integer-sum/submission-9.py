9bead3014a352113ac4cdadd1bf3b1291acbfef3
Ivan <ivan.shelonik@gmail.com>
2026-08-21 13:11:30 +0200

    Add: two-integer-sum - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum/submission-9.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # is array sorted? - Yes; but you should sort it by yourself
+        # answer always exists - no, return None
+
+        pairs = sorted((num, i) for i, num in enumerate(nums))
+
+        l, r = 0, len(pairs) - 1
+        
+        while l < r:
+            s = pairs[l][0] + pairs[r][0]
+            if s > target:
+                r -= 1
+            elif s < target:
+                l += 1
+            else:
+                # Return the answer with the smaller index first.
+                return sorted([pairs[l][1], pairs[r][1]])
+        return None
+
+        
